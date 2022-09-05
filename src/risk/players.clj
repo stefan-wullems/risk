42b5728cebeecfc1db919142157edbf5825a1cf1
Stefan Wullems <stefan.wullems@prima.it>
2022-09-05 14:09:54 +0100

    add allowed actions query

--- src/risk/players.clj
+++ src/risk/players.clj
@@ -2,3 +2,12 @@
 
 (defn current-player [players-state]
   (get-in players-state [:turn :player]))
+
+(def allowed-actions-by-previous-action
+  {nil #{:distribute-armies}
+   :distribute-armies #{:attack :fortify-positions :end-turn}
+   :attack #{:attack :fortify-positions :end-turn}})
+
+(defn allowed-actions [player-state]
+  (let [previous-action (get-in player-state [:turn :previous-action])]
+    (get allowed-actions-by-previous-action previous-action)))
--- test/risk/players_test.clj
+++ test/risk/players_test.clj
@@ -5,4 +5,15 @@
 (deftest current-player
   (testing "Returns current player"
     (let [player-state {:turn {:player :green}}]
-      (is (= :green (players/current-player player-state))))))
+      (is (= (players/current-player player-state) :green)))))
+
+(deftest allowed-actions
+  (testing "Allow only distributing armies when a player has not acted yet"
+    (let [player-state {:turn {:player :green}}]
+      (is (=  (players/allowed-actions player-state) #{:distribute-armies}))))
+  (testing "Allow attacking, fortifying positions or ending ones turn when a player has distributed armies"
+    (let [player-state {:turn {:player :green :previous-action :distribute-armies}}]
+      (is (=  (players/allowed-actions player-state) #{:attack, :fortify-positions, :end-turn}))))
+  (testing "Allow attacking, fortifying positions or ending ones turn when a player has attacked"
+    (let [player-state {:turn {:player :green :previous-action :attack}}]
+      (is (=  (players/allowed-actions player-state) #{:attack, :fortify-positions, :end-turn})))))
