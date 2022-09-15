8fec22f0324e6615e0214c8035c166a8cb3282b3
Stefan Wullems <stefan.wullems@prima.it>
2022-09-15 18:21:09 +0100

    delete unused files

--- test/risk/game_test.clj
+++
@@ -1,8 +0,0 @@
-(ns risk.game-test
-  (:require [clojure.test :refer :all]
-            [risk.game :as game]))
-
-(deftest current-player
-  (testing "Returns current player"
-    (let [game-state {:players {:turn {:player :green}}}]
-      (is (= :green (game/current-player game-state))))))
